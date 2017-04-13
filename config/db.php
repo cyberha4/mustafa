47ee13a54f751369f04a9c017b582402db0bec3c
cyberha4 <proffi99@mail.ru>
2017-04-13 11:42:38 +0300

    four

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=proffi38_mustafa',
-    'username' => 'proffi38_mustafa',
-    'password' => '260992',
-    'charset' => 'utf8',
-];
