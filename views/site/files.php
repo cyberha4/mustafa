3842717b53946e06d245e0f9ff9adaa3281a12d1
Cyberha4 <proffi99@mail.ru>
2017-04-13 15:16:22 +0300

    5 from phpstorm

--- views/site/files.php
+++ views/site/files.php
@@ -5,11 +5,13 @@
  * Date: 12.04.2017
  * Time: 7:21
  */
+use yii\helpers\Url;
 
 /** @var \app\models\Files[] $files */
+echo Url::base(true) . '<br>';
 if (!empty($files)) {
     foreach ($files as $file) {
-        echo "<a href = 'http://mustafa.local/uploads/" . $file->position . "'>$file->file_name</a><br>";
+        echo "<a href = '".Url::base(true)."/uploads/" . $file->position . "'>$file->file_name</a><br>";
     }
 } else
     echo 'Файлов в данной категории не существует';
